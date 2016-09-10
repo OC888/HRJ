888072944ad810be6d9d2be8d77a702580619dee
OC888 <data666@yeah.net>
2016-09-10 16:43:57 +0800

    push

---
+++ githubTest/githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by aaa on 16/9/10.
+//  Copyright © 2016年 huarenjieiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/githubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by aaa on 16/9/10.
+//  Copyright © 2016年 huarenjieiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
